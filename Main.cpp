9ab28f6019ebd651fbc28c4c8fcf112b6fbc1bd6
Luca <luca@Federicos-Mac-Studio-Ultra.local>
2025-09-21 21:06:32 -0700

    ADD functionality programmed.

--- Main.cpp
+++ Main.cpp
@@ -5,20 +5,34 @@
 
 using namespace std;
 
-void add(int nStudent, Node* head);
+void add(Student* nStudent, Node* head);
 void print(Node* head);
 
 int main() {
 
   Node* head = NULL;
 
-
+  add(new Student("Luca", 497022), head);
+  add(new Student("Evan", 530232), head);
+  add(new Student("Emily", 457203), head);
   
   return 0;
 }
 
-void add(int nStudent, Node* head) {
+void add(Student* nStudent, Node* head) {
+  Node* current = head;
 
+  if (current == NULL) {
+    head = new Node();
+    head -> setStudent(nStudent);
+  }
+  else {
+    while (current -> getNext() != NULL) {
+      current = current -> getNext();
+    }
+    current -> setNext(new Node());
+    current -> getNext() -> setStudent(nStudent);
+  }
 }
 
 void print(Node* head) {
